f19ffa654b71daf451e808f08acfc37f44252440
Karine Liuti <kah.liuti@gmail.com>
2018-11-08 20:08:05 -0200

    Made axios http request to get items of service; Started layout fixes

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react';
 import {
-  Text
+  Image,
+  StyleSheet,
+  Text,
+  View
 } from 'react-native';
 
 export default class Items extends Component {
   render() {
     return (
-      <Text>Detalhes do item...</Text>
+      <View style={styles.item}>
+        <View style={styles.itemPhoto}>
+          <Image source={{ uri: this.props.listItem.foto }} style={{ width: 100, height: 100 }} />
+        </View>
+        <View style={styles.itemDescriptions}>
+          <Text style={styles.itemTitle}>{this.props.listItem.titulo}</Text>
+          <Text style={styles.itemPrice}>$ {this.props.listItem.valor}</Text>
+          <Text>{this.props.listItem.local_anuncio}</Text>
+          <Text>{this.props.listItem.data_publicacao}</Text>
+        </View>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  item: {
+    borderWidth: 0.5,
+    borderColor: '#FFF',
+    borderBottomColor: '#DCDCDC',
+    borderRightColor: '#DCDCDC',
+    margin: 10,
+    padding: 10,
+    flexDirection: 'row',
+    backgroundColor: '#FFF'
+  },
+  itemDescriptions: {
+    padding: 10,
+    flex: 1
+  },
+  itemTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: 'blue'
+  },
+  itemPhoto: {
+    width: 102,
+    height: 102
+  },
+  itemPrice: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+});
--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,24 +1,34 @@
+
 import React, { Component } from 'react';
 import {
-  View
+  ScrollView,
+  StyleSheet
 } from 'react-native';
-import axios from 'axios';
 import Items from './Items';
+import axios from 'axios';
 
 export default class ListItems extends Component {
-  // componentWillMount() {
-  //   console.log('Vou chamar!');
-  //   axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
-	// 		.then((response) => { console.log(response); })
-	// 		.catch(() => { console.log('Erro ao recuperar os dados'); });
-  // }
+  constructor(props) {
+    super(props);
+    this.state = { listItems: [] };
+  }
+  componentWillMount() {
+    axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+			.then((response) => { this.setState({ listItems: response.data }); })
+			.catch((err) => { console.log('Erro ao recuperar os dados', err); });
+  }
   render() {
     return (
-      <View>
-        <Items />
-        <Items />
-        <Items />
-      </View>
+      <ScrollView style={styles.itemsContent}>
+        { this.state.listItems.map(item => (<Items key={item.titulo} listItem={item} />))}
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  itemsContent: {
+    backgroundColor: '#f9ffff'
+  }
+});
+
